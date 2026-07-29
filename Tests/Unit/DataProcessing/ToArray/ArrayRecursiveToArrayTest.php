5d4c5a3db100c95ef386b3b62778efb20ffbb1b8
Steffen Maechtel <info@steffen-maechtel.de>
2026-07-29 23:36:20 +0200

    Add priority two unit tests

--- Tests/Unit/DataProcessing/ToArray/ArrayRecursiveToArrayTest.php
+++ Tests/Unit/DataProcessing/ToArray/ArrayRecursiveToArrayTest.php
@@ -7,7 +7,20 @@ namespace Netzbewegung\NbHeadlessContentBlocks\Tests\Unit\DataProcessing\ToArray
 use Netzbewegung\NbHeadlessContentBlocks\DataProcessing\ToArray\ArrayRecursiveToArray;
 use Netzbewegung\NbHeadlessContentBlocks\Event\ModifyArrayRecursiveToArrayEvent;
 use Psr\EventDispatcher\ListenerProviderInterface;
+use TYPO3\CMS\ContentBlocks\Definition\Capability\TableDefinitionCapability;
+use TYPO3\CMS\ContentBlocks\Definition\ContentType\ContentType;
+use TYPO3\CMS\ContentBlocks\Definition\ContentType\ContentTypeDefinitionCollection;
+use TYPO3\CMS\ContentBlocks\Definition\PaletteDefinitionCollection;
+use TYPO3\CMS\ContentBlocks\Definition\SqlColumnDefinitionCollection;
+use TYPO3\CMS\ContentBlocks\Definition\TableDefinition;
 use TYPO3\CMS\ContentBlocks\Definition\TableDefinitionCollection;
+use TYPO3\CMS\ContentBlocks\Definition\TcaFieldDefinition;
+use TYPO3\CMS\ContentBlocks\Definition\TcaFieldDefinitionCollection;
+use TYPO3\CMS\ContentBlocks\FieldType\FieldTypeInterface;
+use TYPO3\CMS\ContentBlocks\FieldType\JsonFieldType;
+use TYPO3\CMS\ContentBlocks\FieldType\PasswordFieldType;
+use TYPO3\CMS\ContentBlocks\FieldType\TextareaFieldType;
+use TYPO3\CMS\ContentBlocks\FieldType\TextFieldType;
 use TYPO3\CMS\ContentBlocks\Registry\AutomaticLanguageKeysRegistry;
 use TYPO3\CMS\Core\EventDispatcher\EventDispatcher;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
@@ -116,19 +129,138 @@ final class ArrayRecursiveToArrayTest extends UnitTestCase
         self::assertSame([['myKey', 'myValue']], $receivedEvents);
     }
 
+    public function testJsonFieldIsPreservedWithoutRecursiveProcessing(): void
+    {
+        $subject = $this->createSubject(
+            [
+                'settings' => [
+                    'z' => 1,
+                    'nested' => ['b' => 2, 'a' => 1],
+                ],
+            ],
+            tableDefinition: $this->createTableDefinition(
+                $this->createFieldDefinition('settings', 'publicSettings', new JsonFieldType())
+            )
+        );
+
+        self::assertSame([
+            'publicSettings' => [
+                'z' => 1,
+                'nested' => ['b' => 2, 'a' => 1],
+            ],
+        ], $subject->toArray());
+    }
+
+    public function testTcaFieldIdentifierIsUsedAsOutputKey(): void
+    {
+        $subject = $this->createSubject(
+            ['internalName' => 'value'],
+            tableDefinition: $this->createTableDefinition(
+                $this->createFieldDefinition('internalName', 'publicName', new TextFieldType())
+            )
+        );
+
+        self::assertSame(['publicName' => 'value'], $subject->toArray());
+    }
+
+    public function testPasswordFieldIsEmptiedAndTextFieldIsPassedThrough(): void
+    {
+        $subject = $this->createSubject(
+            [
+                'password' => 'secret',
+                'text' => 'visible',
+            ],
+            tableDefinition: $this->createTableDefinition(
+                $this->createFieldDefinition('password', 'password', new PasswordFieldType()),
+                $this->createFieldDefinition('text', 'text', new TextFieldType())
+            )
+        );
+
+        self::assertSame([
+            'password' => '',
+            'text' => 'visible',
+        ], $subject->toArray());
+    }
+
+    public function testNonRichTextTextareaIsPassedThrough(): void
+    {
+        $subject = $this->createSubject(
+            ['body' => '<strong>visible</strong>'],
+            tableDefinition: $this->createTableDefinition(
+                $this->createFieldDefinition(
+                    'body',
+                    'body',
+                    $this->createTextareaFieldType()
+                )
+            )
+        );
+
+        self::assertSame(['body' => '<strong>visible</strong>'], $subject->toArray());
+    }
+
     /**
      * @param callable[] $listeners
      */
-    private function createSubject(array $array, array $listeners = []): ArrayRecursiveToArray
-    {
+    private function createSubject(
+        array $array,
+        array $listeners = [],
+        ?TableDefinition $tableDefinition = null
+    ): ArrayRecursiveToArray {
         $tableDefinitionCollection = new TableDefinitionCollection(new AutomaticLanguageKeysRegistry());
 
         return new ArrayRecursiveToArray(
             $array,
-            null,
+            $tableDefinition,
             $tableDefinitionCollection,
             $this->createEventDispatcher($listeners)
         );
+    }
+
+    private function createTableDefinition(TcaFieldDefinition ...$fields): TableDefinition
+    {
+        $fieldDefinitionCollection = new TcaFieldDefinitionCollection();
+        foreach ($fields as $field) {
+            $fieldDefinitionCollection->addField($field);
+        }
+
+        return new TableDefinition(
+            'test_table',
+            TableDefinitionCapability::createFromArray([]),
+            'CType',
+            ContentType::CONTENT_ELEMENT,
+            new ContentTypeDefinitionCollection(),
+            new SqlColumnDefinitionCollection(),
+            $fieldDefinitionCollection,
+            new PaletteDefinitionCollection(),
+            []
+        );
+    }
+
+    private function createFieldDefinition(
+        string $uniqueIdentifier,
+        string $identifier,
+        FieldTypeInterface $fieldType
+    ): TcaFieldDefinition {
+        return new TcaFieldDefinition(
+            ContentType::CONTENT_ELEMENT,
+            $identifier,
+            $uniqueIdentifier,
+            '',
+            '',
+            '',
+            false,
+            $fieldType
+        );
+    }
+
+    private function createTextareaFieldType(): TextareaFieldType
+    {
+        $fieldType = (new TextareaFieldType())->createFromArray([]);
+        $fieldType->setName('Textarea');
+        $fieldType->setTcaType('text');
+        $fieldType->setSearchable(true);
+
+        return $fieldType;
     }
 
     /**
