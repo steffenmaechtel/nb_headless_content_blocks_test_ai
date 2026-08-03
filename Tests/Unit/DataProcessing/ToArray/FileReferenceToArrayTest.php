a6b577be79a519ea3649ebc605f3da8e947e5357
Steffen Maechtel <info@steffen-maechtel.de>
2026-08-03 23:51:29 +0200

    snapshot 03

---
+++ Tests/Unit/DataProcessing/ToArray/FileReferenceToArrayTest.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netzbewegung\NbHeadlessContentBlocks\Tests\Unit\DataProcessing\ToArray;
+
+use TYPO3\CMS\Core\Imaging\ImageManipulation\CropVariantCollection;
+use TYPO3\CMS\Core\Resource\File;
+use TYPO3\CMS\Core\Resource\FileReference;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+final class FileReferenceToArrayTest extends UnitTestCase
+{
+    public function testFileReferenceIsConvertedToArray(): void
+    {
+        $file = $this->createMock(File::class);
+        $file->method('getUid')->willReturn(123);
+        $file->method('getAlternative')->willReturn('Alt Text');
+        $file->method('getTitle')->willReturn('Image Title');
+        $file->method('getPublicPath')->willReturn('/public/path/to/image.jpg');
+
+        $fileReference = $this->createMock(FileReference::class);
+        $fileReference->method('getUid')->willReturn(456);
+        $fileReference->method('getProperty')->with('crop')->willReturn('crop:100,200,300,400');
+        $fileReference->method('hasProperty')->with('crop')->willReturn(true);
+        $fileReference->method('getAlternative')->willReturn('Alt Text');
+        $fileReference->method('getTitle')->willReturn('Image Title');
+
+        $converter = new FileReferenceToArray($fileReference);
+        $result = $converter->toArray();
+
+        self::assertArrayHasKey('id', $result);
+        self::assertSame(456, $result['id']);
+        self::assertArrayHasKey('alt', $result);
+        self::assertSame('Alt Text', $result['alt']);
+        self::assertArrayHasKey('title', $result);
+        self::assertSame('Image Title', $result['title']);
+        self::assertArrayHasKey('publicUrl', $result);
+        self::assertStringStartsWith('http', $result['publicUrl']);
+    }
+
+    public function testFileReferenceWithoutCropReturnsPublicUrl(): void
+    {
+        $fileReference = $this->createMock(FileReference::class);
+        $fileReference->method('getUid')->willReturn(789);
+        $fileReference->method('getProperty')->with('crop')->willReturn('');
+        $fileReference->method('hasProperty')->with('crop')->willReturn(false);
+        $fileReference->method('getAlternative')->willReturn('No Alt');
+        $fileReference->method('getTitle')->willReturn('No Title');
+
+        $converter = new FileReferenceToArray($fileReference);
+        $result = $converter->toArray();
+
+        self::assertArrayHasKey('id', $result);
+        self::assertSame(789, $result['id']);
+        self::assertArrayHasKey('publicUrl', $result);
+        self::assertStringStartsWith('http', $result['publicUrl']);
+    }
+
+    public function testFileReferenceWithEmptyCropReturnsPublicUrl(): void
+    {
+        $fileReference = $this->createMock(FileReference::class);
+        $fileReference->method('getUid')->willReturn(101);
+        $fileReference->method('getProperty')->with('crop')->willReturn('');
+        $fileReference->method('hasProperty')->with('crop')->willReturn(true);
+        $fileReference->method('getAlternative')->willReturn('Empty Alt');
+        $fileReference->method('getTitle')->willReturn('Empty Title');
+
+        $converter = new FileReferenceToArray($fileReference);
+        $result = $converter->toArray();
+
+        self::assertArrayHasKey('id', $result);
+        self::assertSame(101, $result['id']);
+        self::assertArrayHasKey('publicUrl', $result);
+    }
+
+    public function testFileReferenceWithNullAltReturnsEmptyString(): void
+    {
+        $fileReference = $this->createMock(FileReference::class);
+        $fileReference->method('getUid')->willReturn(202);
+        $fileReference->method('getAlternative')->willReturn(null);
+        $fileReference->method('getTitle')->willReturn('Title Only');
+
+        $converter = new FileReferenceToArray($fileReference);
+        $result = $converter->toArray();
+
+        self::assertArrayHasKey('alt', $result);
+        self::assertEmpty($result['alt']);
+    }
+
+    public function testFileReferenceWithNullTitleReturnsEmptyString(): void
+    {
+        $fileReference = $this->createMock(FileReference::class);
+        $fileReference->method('getUid')->willReturn(303);
+        $fileReference->method('getAlternative')->willReturn('Alt');
+        $fileReference->method('getTitle')->willReturn(null);
+
+        $converter = new FileReferenceToArray($fileReference);
+        $result = $converter->toArray();
+
+        self::assertArrayHasKey('title', $result);
+        self::assertEmpty($result['title']);
+    }
+}
