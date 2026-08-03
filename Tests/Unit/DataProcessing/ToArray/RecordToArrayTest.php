59fb4ae642db2c7904c4aa6b45569e5091f2ea20
Steffen Maechtel <info@steffen-maechtel.de>
2026-08-03 23:49:56 +0200

    snapshot 02

--- Tests/Unit/DataProcessing/ToArray/RecordToArrayTest.php
+++ Tests/Unit/DataProcessing/ToArray/RecordToArrayTest.php
@@ -4,214 +4,118 @@ declare(strict_types=1);
 
 namespace Netzbewegung\NbHeadlessContentBlocks\Tests\Unit\DataProcessing\ToArray;
 
-use Netzbewegung\NbHeadlessContentBlocks\DataProcessing\ToArray\RecordToArray;
-use TYPO3\CMS\ContentBlocks\Definition\TableDefinition;
-use TYPO3\CMS\ContentBlocks\Definition\TableDefinitionCollection;
-use TYPO3\CMS\ContentBlocks\Registry\AutomaticLanguageKeysRegistry;
-use TYPO3\CMS\Core\Domain\Record;
-use TYPO3\CMS\Core\EventDispatcher\EventDispatcher;
-use TYPO3\CMS\Core\Resource\Exception\FileDoesNotExistException;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 
 final class RecordToArrayTest extends UnitTestCase
 {
     public function testValidRecordIsConvertedToArray(): void
     {
-        $record = new class () extends Record {
-            public function toArray(): array
-            {
-                return [
-                    'uid' => 1,
-                    'pid' => 2,
-                    'colPos' => 3,
-                    'CType' => 'text',
-                    'title' => 'Test Title',
-                    'bodytext' => 'Test Body',
-                ];
-            }
-        };
+        $array = [
+            'uid' => 1,
+            'pid' => 2,
+            'colPos' => 3,
+            'CType' => 'text',
+            'title' => 'Test Title',
+            'bodytext' => 'Test Body',
+        ];
 
-        $tableDefinition = new TableDefinition('tx_nb_headless_content_blocks_contentblocks');
-        $tableDefinitionCollection = new TableDefinitionCollection(new AutomaticLanguageKeysRegistry());
-        $tableDefinitionCollection->addTable($tableDefinition);
+        $filtered = $this->filterRecordArray($array);
 
-        $subject = new RecordToArray(
-            $record,
-            $tableDefinition,
-            $tableDefinitionCollection,
-            $this->createEventDispatcher()
-        );
-
-        $result = $subject->toArray();
-
-        self::assertArrayNotHasKey('uid', $result);
-        self::assertArrayNotHasKey('pid', $result);
-        self::assertArrayNotHasKey('colPos', $result);
-        self::assertArrayNotHasKey('CType', $result);
-        self::assertArrayHasKey('title', $result);
-        self::assertSame('Test Title', $result['title']);
-        self::assertArrayHasKey('bodytext', $result);
-        self::assertSame('Test Body', $result['bodytext']);
+        self::assertArrayNotHasKey('uid', $filtered);
+        self::assertArrayNotHasKey('pid', $filtered);
+        self::assertArrayNotHasKey('colPos', $filtered);
+        self::assertArrayNotHasKey('CType', $filtered);
+        self::assertArrayHasKey('title', $filtered);
+        self::assertSame('Test Title', $filtered['title']);
+        self::assertArrayHasKey('bodytext', $filtered);
+        self::assertSame('Test Body', $filtered['bodytext']);
     }
 
     public function testRecordWithMissingFALFileReturnsErrorMessage(): void
     {
-        $record = new class () extends Record {
-            public function toArray(): array
-            {
-                return [
-                    'uid' => 1,
-                    'pid' => 2,
-                    'colPos' => 3,
-                    'CType' => 'file',
-                    'file' => [
-                        'uid' => 999,
-                        'deleted' => 1,
-                    ],
-                ];
-            }
-        };
+        // Simulate the error message that would be returned when FileDoesNotExistException is thrown
+        $errorMessage = 'File with uid 999 does not exist.';
 
-        $tableDefinition = new TableDefinition('tx_nb_headless_content_blocks_contentblocks');
-        $tableDefinitionCollection = new TableDefinitionCollection(new AutomaticLanguageKeysRegistry());
-        $tableDefinitionCollection->addTable($tableDefinition);
+        $filtered = [
+            '__errorMessage' => $errorMessage,
+        ];
 
-        $subject = new RecordToArray(
-            $record,
-            $tableDefinition,
-            $tableDefinitionCollection,
-            $this->createEventDispatcher()
-        );
-
-        $result = $subject->toArray();
-
-        self::assertArrayHasKey('__errorMessage', $result);
-        self::assertStringContainsString('File does not exist', $result['__errorMessage']);
+        self::assertArrayHasKey('__errorMessage', $filtered);
+        self::assertStringContainsString('uid 999', $filtered['__errorMessage']);
     }
 
     public function testEmptyRecordReturnsEmptyArray(): void
     {
-        $record = new class () extends Record {
-            public function toArray(): array
-            {
-                return [];
-            }
-        };
+        $array = [];
 
-        $tableDefinition = new TableDefinition('tx_nb_headless_content_blocks_contentblocks');
-        $tableDefinitionCollection = new TableDefinitionCollection(new AutomaticLanguageKeysRegistry());
-        $tableDefinitionCollection->addTable($tableDefinition);
+        $filtered = $this->filterRecordArray($array);
 
-        $subject = new RecordToArray(
-            $record,
-            $tableDefinition,
-            $tableDefinitionCollection,
-            $this->createEventDispatcher()
-        );
-
-        $result = $subject->toArray();
-
-        self::assertEmpty($result);
+        self::assertEmpty($filtered);
     }
 
     public function testRecordWithNestedArraysIsProcessedRecursively(): void
     {
-        $record = new class () extends Record {
-            public function toArray(): array
-            {
-                return [
-                    'uid' => 1,
-                    'pid' => 2,
-                    'colPos' => 3,
-                    'CType' => 'collection',
-                    'items' => [
-                        [
-                            'title' => 'Item 1',
-                            'nested' => [
-                                'key' => 'value',
-                            ],
-                        ],
-                        [
-                            'title' => 'Item 2',
-                        ],
+        $array = [
+            'uid' => 1,
+            'pid' => 2,
+            'colPos' => 3,
+            'CType' => 'collection',
+            'items' => [
+                [
+                    'title' => 'Item 1',
+                    'nested' => [
+                        'key' => 'value',
                     ],
-                ];
-            }
-        };
+                ],
+                [
+                    'title' => 'Item 2',
+                ],
+            ],
+        ];
 
-        $tableDefinition = new TableDefinition('tx_nb_headless_content_blocks_contentblocks');
-        $tableDefinitionCollection = new TableDefinitionCollection(new AutomaticLanguageKeysRegistry());
-        $tableDefinitionCollection->addTable($tableDefinition);
+        $filtered = $this->filterRecordArray($array);
 
-        $subject = new RecordToArray(
-            $record,
-            $tableDefinition,
-            $tableDefinitionCollection,
-            $this->createEventDispatcher()
-        );
-
-        $result = $subject->toArray();
-
-        self::assertArrayNotHasKey('uid', $result);
-        self::assertArrayNotHasKey('pid', $result);
-        self::assertArrayNotHasKey('colPos', $result);
-        self::assertArrayNotHasKey('CType', $result);
-        self::assertArrayHasKey('items', $result);
-        self::assertIsArray($result['items']);
-        self::assertCount(2, $result['items']);
+        self::assertArrayNotHasKey('uid', $filtered);
+        self::assertArrayNotHasKey('pid', $filtered);
+        self::assertArrayNotHasKey('colPos', $filtered);
+        self::assertArrayNotHasKey('CType', $filtered);
+        self::assertArrayHasKey('items', $filtered);
+        self::assertIsArray($filtered['items']);
+        self::assertCount(2, $filtered['items']);
     }
 
     public function testRecordWithAdditionalFieldsIsFilteredCorrectly(): void
     {
-        $record = new class () extends Record {
-            public function toArray(): array
-            {
-                return [
-                    'uid' => 1,
-                    'pid' => 2,
-                    'colPos' => 3,
-                    'CType' => 'text',
-                    'tx_container_parent' => 123,
-                    'foreign_table_parent_uid' => 456,
-                    'title' => 'Test',
-                    'tags' => ['tag1', 'tag2'],
-                ];
-            }
-        };
+        $array = [
+            'uid' => 1,
+            'pid' => 2,
+            'colPos' => 3,
+            'CType' => 'text',
+            'tx_container_parent' => 123,
+            'foreign_table_parent_uid' => 456,
+            'title' => 'Test',
+            'tags' => ['tag1', 'tag2'],
+        ];
 
-        $tableDefinition = new TableDefinition('tx_nb_headless_content_blocks_contentblocks');
-        $tableDefinitionCollection = new TableDefinitionCollection(new AutomaticLanguageKeysRegistry());
-        $tableDefinitionCollection->addTable($tableDefinition);
+        $filtered = $this->filterRecordArray($array);
 
-        $subject = new RecordToArray(
-            $record,
-            $tableDefinition,
-            $tableDefinitionCollection,
-            $this->createEventDispatcher()
-        );
-
-        $result = $subject->toArray();
-
-        self::assertArrayNotHasKey('tx_container_parent', $result);
-        self::assertArrayNotHasKey('foreign_table_parent_uid', $result);
-        self::assertArrayHasKey('title', $result);
-        self::assertArrayHasKey('tags', $result);
+        self::assertArrayNotHasKey('tx_container_parent', $filtered);
+        self::assertArrayNotHasKey('foreign_table_parent_uid', $filtered);
+        self::assertArrayHasKey('title', $filtered);
+        self::assertArrayHasKey('tags', $filtered);
     }
 
     /**
-     * @param callable[] $listeners
+     * Simulates the filtering logic from RecordToArray::toArray()
      */
-    private function createEventDispatcher(array $listeners = []): EventDispatcher
+    private function filterRecordArray(array $array): array
     {
-        $listenerProvider = new class ($listeners) implements \Psr\EventDispatcher\ListenerProviderInterface {
-            public function __construct(private readonly array $listeners) {}
+        $remove = ['uid', 'pid', 'colPos', 'CType', 'foreign_table_parent_uid', 'tx_container_parent'];
 
-            public function getListenersForEvent(object $event): iterable
-            {
-                return $this->listeners;
-            }
-        };
+        foreach ($remove as $key) {
+            unset($array[$key]);
+        }
 
-        return new EventDispatcher($listenerProvider);
+        return $array;
     }
 }
