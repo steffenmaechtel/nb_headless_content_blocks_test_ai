887fa61c9976e6080687f22b56cd8ea188f66a50
Steffen Maechtel <info@steffen-maechtel.de>
2026-08-03 23:53:02 +0200

    snapshot 06

---
+++ Tests/Unit/DataProcessing/ToArray/LazyFolderCollectionToArrayTest.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netzbewegung\NbHeadlessContentBlocks\Tests\Unit\DataProcessing\ToArray;
+
+use TYPO3\CMS\Core\Resource\Collection\LazyFolderCollection;
+use TYPO3\CMS\Core\Resource\StorageRepository;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+final class LazyFolderCollectionToArrayTest extends UnitTestCase
+{
+    public function testLazyFolderCollectionIsConvertedToArray(): void
+    {
+        $storageConfig = ['basePath' => '/public/'];
+        $storage1 = $this->createMock(\TYPO3\CMS\Core\Resource\Storage::class);
+        $storage1->method('getConfiguration')->willReturn($storageConfig);
+        $storage1->method('getIdentifier')->willReturn('storage1');
+
+        $storage2 = $this->createMock(\TYPO3\CMS\Core\Resource\Storage::class);
+        $storage2->method('getConfiguration')->willReturn($storageConfig);
+        $storage2->method('getIdentifier')->willReturn('storage2');
+
+        $folder1 = $this->createMock(\TYPO3\CMS\Core\Resource\Folder::class);
+        $folder1->method('getStorage')->willReturn($storage1);
+        $folder1->method('getIdentifier')->willReturn('folder1');
+
+        $folder2 = $this->createMock(\TYPO3\CMS\Core\Resource\Folder::class);
+        $folder2->method('getStorage')->willReturn($storage2);
+        $folder2->method('getIdentifier')->willReturn('folder2');
+
+        $lazyCollection = $this->createMock(LazyFolderCollection::class);
+        $lazyCollection->method('__invoke')->willReturnOnConsecutiveCalls($folder1, $folder2);
+
+        $converter = new LazyFolderCollectionToArray($lazyCollection);
+        $result = $converter->toArray();
+
+        self::assertCount(2, $result);
+        self::assertArrayHasKey(0, $result);
+        self::assertArrayHasKey(1, $result);
+        self::assertSame('/public/folder1', $result[0]);
+        self::assertSame('/public/folder2', $result[1]);
+    }
+
+    public function testEmptyLazyFolderCollectionReturnsEmptyArray(): void
+    {
+        $lazyCollection = $this->createMock(LazyFolderCollection::class);
+        $lazyCollection->method('__invoke')->willReturn(null);
+
+        $converter = new LazyFolderCollectionToArray($lazyCollection);
+        $result = $converter->toArray();
+
+        self::assertEmpty($result);
+    }
+
+    public function testLazyFolderCollectionWithNullValuesIsHandled(): void
+    {
+        $storageConfig = ['basePath' => '/public/'];
+        $storage1 = $this->createMock(\TYPO3\CMS\Core\Resource\Storage::class);
+        $storage1->method('getConfiguration')->willReturn($storageConfig);
+        $storage1->method('getIdentifier')->willReturn('storage1');
+
+        $folder1 = $this->createMock(\TYPO3\CMS\Core\Resource\Folder::class);
+        $folder1->method('getStorage')->willReturn($storage1);
+        $folder1->method('getIdentifier')->willReturn('folder1');
+
+        $lazyCollection = $this->createMock(LazyFolderCollection::class);
+        $lazyCollection->method('__invoke')->willReturnOnConsecutiveCalls($folder1, null);
+
+        $converter = new LazyFolderCollectionToArray($lazyCollection);
+        $result = $converter->toArray();
+
+        self::assertCount(2, $result);
+        self::assertArrayHasKey(0, $result);
+        self::assertArrayHasKey(1, $result);
+    }
+}
